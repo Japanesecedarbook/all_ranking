1dc61eee0fdbbef82258d41954061a3eb7ebae02
Japanesecedarbook <yositakasugi1113@gmail.com>
2020-06-25 19:50:32 +0900

    add user-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "rankings#index"
+  resources :users, only: [:edit, :update]
   resources :rankings, only: [:index, :new, :create, :edit, :update, :destroy]
 end
