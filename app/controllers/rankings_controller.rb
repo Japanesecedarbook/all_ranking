13a54c67d696b955363b7eda4ef4a0b0ea4f5e36
Japanesecedarbook <yositakasugi1113@gmail.com>
2020-06-25 17:26:39 +0900

    add ranking form error

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,6 @@
 class RankingsController < ApplicationController
   def index
-    @rankings = Ranking.all
+    @rankings = Ranking.all.order(id: "DESC")
   end
 
   def new
@@ -8,12 +8,17 @@ class RankingsController < ApplicationController
   end
 
   def create
-    Ranking.create(ranking_params)
+    # Ranking.create(ranking_params)
+    @ranking = Ranking.new(ranking_params)
+    if @ranking.save
+      redirect_to controller: :rankings, action: :index
+    else
+      render "new"
+    end
   end
 
   private
   def ranking_params
     params.require(:ranking).permit(:title, :rank1, :rank2, :rank3, :image).merge(user_id: current_user.id)
   end
-
 end
