2b2a2315d8475cf5abef33d34e2b6ff12ccaab4b
Japanesecedarbook <yositakasugi1113@gmail.com>
2020-08-06 09:50:31 +0900

    ruby ver修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-lock '3.14.1'
+lock "~> 3.14.1"
 
 set :application, 'all_ranking'
 set :repo_url,  'git@github.com:Japanesecedarbook/all_ranking.git'
@@ -6,7 +6,7 @@ set :repo_url,  'git@github.com:Japanesecedarbook/all_ranking.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.6.5'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/sugiyoshi.pem']
